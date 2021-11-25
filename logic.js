43d212b6c1538a7bea7508f6373381fcf456cc88
Sviatoslav Chapcha <65873134+whitetark@users.noreply.github.com>
2021-11-25 18:05:12 +0200

    Second Commit (Update)
    Added 5th task
    Some visual improvements
    Cookie fix

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
-let sctn_3 = document.getElementById("box3");
-let sctn_6 = document.getElementById("box6");
-let sctn_5 = document.getElementById("box5");
-let sctn_4_cont = document.getElementById("box4").childNodes[1];
-var count = 0;
-let button = document.getElementById("enterbutton");
+let sctn_3 = document.getElementById("box3"); //for 1st task
+let sctn_6 = document.getElementById("box6"); //for 1st task
+let sctn_5 = document.getElementById("box5"); //for 2nd task
+let sctn_4_cont = document.getElementById("box4").childNodes[1]; //for 4th task
+let sctn_4 = document.getElementById("box4"); //for 5th task
+let altbox = document.getElementById("x"); //for 5th task
 
 // ======#1======
 sctn_3.onclick = function(event){
@@ -26,6 +26,7 @@ function Swapp()
 }
 
 // ======#2======
+var count = 0;
 sctn_5.childNodes[1].onclick = function(event){
     if(count < 4)
     {
@@ -48,6 +49,8 @@ function Area()
 }
 
 // ======#3======
+let button = document.getElementById("enterbutton");
+
 window.onload = reloadCookie();
 function reloadCookie(){
     if(getCookie("cookie1")){
@@ -115,7 +118,7 @@ function dividersSearch(){
     }
     var result = "";
     arr.forEach(element => {
-        result += ":" + element;
+        result += " " + element;
     });
 
     alert(result);
@@ -168,3 +171,110 @@ function Start(){
 Start();
 
 // ======#5======
+var already_generated = 0;
+altbox.onclick = function(event){
+   if(already_generated == 0){
+    GenEntryField();
+        already_generated++;
+   }
+}
+
+function GenEntryField(){
+    var tag1 = document.createElement("form");
+    tag1.name = "imgforms";
+    tag1.action="";
+
+    var tag2 = document.createElement("input");
+    tag2.type = "button";
+    tag2.id = "saveBtn";
+    tag2.value = "Save";
+    
+    var tag3 = document.createElement("input");
+    tag3.type = "text";
+    tag3.id = "urlpic";
+    
+    var tag4 = document.createElement("input");
+    tag4.type = "button";
+    tag4.id = "showBtn";
+    tag4.value = "Show All";
+
+    tag1.appendChild(tag2);
+    tag1.appendChild(tag3);
+    tag1.appendChild(tag4);
+    sctn_5.appendChild(tag1);
+
+    var div1 = document.createElement("div");
+    div1.style = "display: flex; flex-direction: column;";
+    div1.id = "album";    
+    sctn_4.appendChild(div1);
+
+    tag2.onclick = function(event){
+        SavePicture(tag3, div1);
+    }
+
+    tag4.onclick = function(event){
+        ShowAllPics(div1);
+    }
+}
+
+function SavePicture(url, div1){
+    var div2 = document.createElement("div");
+    div2.style = "display: flex; flex-direction: column; align-items: center";   
+    var p = document.createElement("p");
+    var img1 = document.createElement("img");
+
+    div2.appendChild(p);
+    
+    img1.src = url.value;
+    img1.alt = "Image Not Found";
+    img1.style = "height: 250px; width: 250px;"
+    div2.appendChild(img1);
+    url.value = "";
+    var urimg = localStorage.getItem("pic")+"@"+img1.src;
+    localStorage.setItem("pic", urimg);
+
+    var deletebut = document.createElement("button");
+    deletebut.name = "delbut";
+    deletebut.textContent = "Delete Photo"
+    deletebut.style= "background-color: #101010; color: white; border-radius: 5px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); width:100px; height:25px;"
+    deletebut.id = img1.src;
+    div2.appendChild(deletebut);
+    div1.appendChild(div2);
+
+    deletebut.onclick = function(event){
+        deletebut.parentNode.remove();
+        localStorage.setItem("pic", localStorage.getItem("pic").replace('@'+deletebut.id,''));
+    }
+}
+
+function ShowAllPics(div1){
+    div1.innerHTML = '';
+    var arr = localStorage.getItem("pic").split('@');
+    arr.forEach(element => {
+        if(element != 'null'){            
+            var p = document.createElement("p");
+            var div2 = document.createElement("div");
+            div2.style = "display: flex; flex-direction: column; align-items: center";   
+
+            div2.appendChild(p);
+            var img1 = document.createElement("img");
+            img1.src = element;
+            img1.alt = "Image Not Found";
+            img1.style = "height: 250px; width: 250px;"
+            div2.appendChild(img1);
+
+            var deletebut = document.createElement("button");
+            deletebut.name = "delbut";
+            deletebut.textContent = "Delete Photo"
+            deletebut.id = img1.src;
+            deletebut.style= "background-color: #101010; color: white; border-radius: 5px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); width:100px; height:25px;"
+            div2.appendChild(deletebut);
+            div1.appendChild(div2);
+
+            deletebut.onclick = function(event){
+                deletebut.parentNode.remove();
+                localStorage.setItem("pic", localStorage.getItem("pic").replace('@'+deletebut.id,''));
+            }
+        }
+    });
+}
